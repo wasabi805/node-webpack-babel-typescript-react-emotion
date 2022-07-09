c963541154f1cd316943978ba07eb6f0ab4dc78d
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-07-09 16:54:11 -0700

    add script for prettier.

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,17 @@
-import express from 'express';
-import Users from './src/routes/users'
+import express from "express";
+import Users from "./src/routes/users";
 
 const app = express();
 const port = 5000;
 
 /* Assets */
-app.use(express.static( 'public'))
+app.use(express.static("public"));
 
 /* Routes */
-app.use('/users', Users);
+app.use("/users", Users);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Root Page');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Root Page");
 });
 
 app.listen(port, () => {
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,29 +1,29 @@
-import express from 'express';
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 
-interface iUsers{
-    id: number
-    name: string
-    ocupation: string
+interface iUsers {
+  id: number;
+  name: string;
+  ocupation: string;
 }
 
 const users: iUsers[] = [
-    {id: 1, name: 'Matt Murdock' , ocupation:'Lawyer'}, 
-    {id: 2, name: 'Jennifer Walters' , ocupation:'Attorney'},
-    {id: 3, name: 'Peter Parker' , ocupation:'Photographer'}
-]
+  { id: 1, name: "Matt Murdock", ocupation: "Lawyer" },
+  { id: 2, name: "Jennifer Walters", ocupation: "Attorney" },
+  { id: 3, name: "Peter Parker", ocupation: "Photographer" },
+];
 
-router.get('/', (req, res)=>{
-    res.send(users)
-})
+router.get("/", (req, res) => {
+  res.send(users);
+});
 
-router.get('/:id',(req, res)=>{
-    const {id} = req.params
-    const userId = parseInt(id, 10)
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const userId = parseInt(id, 10);
 
-    const user = users.filter((user)=>user.id === userId)
-    
-    res.send(user)
-})
+  const user = users.filter((user) => user.id === userId);
 
-export default router
+  res.send(user);
+});
+
+export default router;
