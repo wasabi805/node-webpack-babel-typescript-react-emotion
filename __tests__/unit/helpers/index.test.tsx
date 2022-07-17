9fc059e3363fd8aeb04f933df86f0690c092eba7
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-07-17 15:43:26 -0700

    saving, added test for delete user api call.

--- __tests__/unit/helpers/index.test.tsx
+++ __tests__/unit/helpers/index.test.tsx
@@ -2,9 +2,15 @@
 
 import { callApi } from "../../../src/utils/helpers";
 import fetch from 'jest-fetch-mock'; //https://github.com/jefflau/jest-fetch-mock/issues/82#issuecomment-415772677
-import {mockState} from  '../../utils/test-utils'
+import {mockState, cleanup} from  '../../utils/test-utils'
 
-describe('Helper Fns',()=>{
+describe('callApi',()=>{
+
+    beforeEach(()=>{
+        fetch.resetMocks()
+    })
+
+    afterEach(cleanup)
     
     it('returns all users', async()=>{
         fetch.mockResponseOnce(JSON.stringify(mockState))
@@ -18,4 +24,27 @@ describe('Helper Fns',()=>{
             {"headers": {"Content-Type": "application/json"}, "method": "GET", "url": "http://localhost:5000/users"}
         )
     })
+
+
+    it('deletes users', async()=>{
+        fetch.mockResponseOnce(JSON.stringify([{id: '2', name: 'kayla'}]))
+
+        await callApi({
+            method : 'DELETE',
+            url: 'http://localhost:5000/users/delete-user',
+            body: { id : '1' },
+        })
+        expect(fetch).toHaveBeenCalledTimes(1)
+
+        expect(fetch).toHaveBeenCalledWith(
+            'http://localhost:5000/users/delete-user',
+            {"headers": {"Content-Type": "application/json"},
+             "method": "DELETE", 
+             "url": "http://localhost:5000/users/delete-user", 
+             "body": "{\"id\":\"1\"}",
+            }
+        )
+
+    })
+
 })
